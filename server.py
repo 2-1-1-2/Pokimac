410ec63a3d2b96a62ce274e99ccb1d2cc33a304a
root <root@SARA-EZEL>
2023-06-01 23:15:06 +0200

    test sara

--- server.py
+++ server.py
@@ -199,7 +199,6 @@ def formEquipe():
 def ajouterEquipe():
     pokimac = request.json["pokimac"]
     mycursor = mydb.cursor()
-    pokimac["nom"] = requestSelect_From("nom", pokimac["nom"])
     mycursor.execute("""INSERT INTO equipe_dresseurs (nom) VALUES (%s)""", (pokimac["nom"]))
     mydb.commit()
     mycursor.close()
